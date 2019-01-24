e89ee435187dc22e8a680833ccd7f6dbda1eb701
James Sears <james.hn.sears@gmail.com>
2019-01-24 14:47:22 +0000

    jmeter wip

--- start.sh
+++ start.sh
@@ -6,3 +6,4 @@ docker-compose -f docker-compose.yml up -d --scale xqa-shard=2
 
 docker run -d --net="xqa-query-balancer_xqa" --name="xqa-ingest" -v $HOME/GIT_REPOS/xqa-test-data:/xml jameshnsears/xqa-ingest:latest -message_broker_host xqa-message-broker -path /xml
 
+docker-compose logs -f xqa-shard | grep "size="
